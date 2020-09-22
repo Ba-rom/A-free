3454db463d3230d8f0e7c16b2dd09d3ebc6326d6
unknown <csykik@naver.com>
2020-09-22 19:00:28 +0900

    final

--- Action/action_enum.py
+++ Action/action_enum.py
@@ -5,15 +5,6 @@ class Actions(Enum):
     """
     Actions enum
     """
-    # framewise_recognition.h5
-    # squat = 0
-    # stand = 1
-    # walk = 2
-    # wave = 3
-
-    # framewise_recognition_under_scene.h5
-    stand = 0
-    walk = 1
-    operate = 2
-    fall_down = 3
-    # run = 4
+    etc = 0
+    body_kick = 1
+    head_kick = 2
